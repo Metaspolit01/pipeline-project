2b3d02bd166c33d19a91275555d05f64d379461f
Metaspolit01 <kandykarthik4@gmail.com>
2025-06-01 16:04:13 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ resource "null_resource" "generate_inventory" {
   provisioner "local-exec" {
     command = <<EOT
       echo "[ec2_instance]" > inventory.ini
-      echo "${aws_instance.ec2.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/mine-laptop.pem" >> inventory.ini
+      echo "${aws_instance.ec2.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/runner/.ssh/mine-laptop.pem" >> inventory.ini
     EOT
   }
 
