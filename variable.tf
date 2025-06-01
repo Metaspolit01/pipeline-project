a74d2c598e3b7ce0c95fd04c80b5f54a67814c57
Metaspolit01 <kandykarthik4@gmail.com>
2025-06-01 15:45:38 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -26,6 +26,6 @@ variable "aws_region" {
 variable "key_name" {
   description = "The name of the SSH key pair to use for the EC2 instances"
   type        = string
-  default     = "mine"
+  default     = "mine-laptop"
   
 }
